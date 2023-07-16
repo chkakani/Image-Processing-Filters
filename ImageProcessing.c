e89cca4a49e1fda5bd6b5ca2351b9824bc96fa6e
Harshitha Kakani <harshithakakani@Harshithas-MacBook-Air.local>
2023-07-16 00:32:01 -0700

    t

---
+++ ImageProcessing.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+// Include necessary header files for image format
+
+// Function to read the input image
+
+// Function to write the processed image
+
+// Function to apply the grayscale filter
+
+// Function to apply the blur filter
+
+// Function to apply the sharpening filter
+
+// Function to apply the edge detection filter
+
+// Function to resize the image
+
+int main() {
+    // Read the input image
+    
+    // Apply desired filters
+    
+    // Write the processed image
+    
+    // Clean up
+    
+    return 0;
+}
+
---
+++ ImageProcessingFunctions.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+// Include necessary header files for image format
+
+// Function to read the input image
+
+// Function to write the processed image
+
+// Function to apply the grayscale filter
+
+// Function to apply the blur filter
+
+// Function to apply the sharpening filter
+
+// Function to apply the edge detection filter
+
+// Function to resize the image
